b984e7feb48ba54c049de55b77c9474f8756875f
bjosue-bf <bjosue.bf@gmail.com>
2025-02-22 16:34:43 +0000

    Update script.js
    Signed-off-by: bjosue-bf <bjosue.bf@gmail.com>

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-// Fonction pour gérer le défilement fluide
-function faireDefilerVersSection(id) {
-    const section = document.getElementById(id);
-    if (section) {
-        section.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
-}
-
-// Ajouter des écouteurs d'événements aux liens du menu
-document.querySelectorAll('nav a').forEach(lien => {
-    lien.addEventListener('click', (event) => {
-        event.preventDefault(); // Empêcher le comportement par défaut du lien
-        const cible = lien.getAttribute('href').substring(1); // Récupérer l'ID de la section
-        faireDefilerVersSection(cible);
-
-        // Fermer le menu mobile après avoir cliqué sur un lien
-        if (window.innerWidth <= 768) {
-            const nav = document.querySelector('nav');
-            const hamburger = document.querySelector('.hamburger');
-            nav.classList.remove('active');
-            hamburger.classList.remove('open');
-        }
-    });
-});
-
-// Gestion du menu hamburger
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav');
-
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    menuToggle.classList.toggle('open');
-});
-
 // Fonction pour afficher les sections au défilement
 function afficherSectionsAuDefilement() {
     const sections = document.querySelectorAll('.section');
@@ -51,3 +17,22 @@ window.addEventListener('scroll', afficherSectionsAuDefilement);
 
 // Appliquer l'affichage des sections au chargement de la page
 window.addEventListener('load', afficherSectionsAuDefilement);
+
+// Gestion du menu hamburger
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    menuToggle.classList.toggle('open');
+});
+
+// Fermer le menu mobile après avoir cliqué sur un lien
+document.querySelectorAll('nav a').forEach(lien => {
+    lien.addEventListener('click', () => {
+        if (window.innerWidth <= 768) {
+            nav.classList.remove('active');
+            menuToggle.classList.remove('open');
+        }
+    });
+});
